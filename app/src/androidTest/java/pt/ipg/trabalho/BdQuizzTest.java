637435d9f1cbd5d2cf01d4afdd9bb9800dd574d1
Pedromatos98 <32634089+Pedromatos98@users.noreply.github.com>
2019-05-20 02:21:54 +0100

    teste cRUD categorias

--- app/src/androidTest/java/pt/ipg/trabalho/BdQuizzTest.java
+++ app/src/androidTest/java/pt/ipg/trabalho/BdQuizzTest.java
@@ -38,7 +38,92 @@ public class BdQuizzTest {
         assertTrue(db.isOpen());
     }
 
-    private Context getAppContext() {
-        return InstrumentationRegistry.getTargetContext();
+    private Context getAppContext() { return InstrumentationRegistry.getTargetContext();
+    }
+
+    @Test
+    public void testCRUD() {
+        BdQuizzOpenHelper openHelper = new BdQuizzOpenHelper(getAppContext());
+        SQLiteDatabase db = openHelper.getWritableDatabase();
+
+        BdTableCategories tabelaCategorias = new BdTableCategories(db);
+
+        // Teste read categorias (cRud)
+        Cursor cursorCategorias = getCategorias(tabelaCategorias);
+        assertEquals(0, cursorCategorias.getCount());
+        // Teste create/read categorias (CRud)
+        String nome = "História";
+        long idHistoria= criaCategoria(tabelaCategorias, nome);
+
+        cursorCategorias = getCategorias(tabelaCategorias);
+        assertEquals(1, cursorCategorias.getCount());
+
+        Categories categoria = getCategoriaComID(cursorCategorias, idHistoria);
+
+        assertEquals(nome, categoria.getDescricao());
+
+        nome = "Desporto";
+        long idDesporto = criaCategoria(tabelaCategorias, nome);
+
+        cursorCategorias = getCategorias(tabelaCategorias);
+        assertEquals(2, cursorCategorias.getCount());
+
+        categoria = getCategoriaComID(cursorCategorias, idDesporto);
+
+        assertEquals(nome, categoria.getDescricao());
+
+        // Teste Update/Read categorias (cRUd)
+        nome = "História / Desporto";
+        categoria.setDescricao(nome);
+
+        int registosAlterados = tabelaCategorias.update(categoria.getContentValues(), BdTableCategories._ID + "=?", new String[]{String.valueOf(idHistoria)});
+
+        assertEquals(1, registosAlterados);
+
+        cursorCategorias = getCategorias(tabelaCategorias);
+        categoria = getCategoriaComID(cursorCategorias, idHistoria);
+
+        assertEquals(nome, categoria.getDescricao());
+
+        // Teste Create/Delete/Read categorias (CRuD)
+        long id = criaCategoria(tabelaCategorias, "TESTE");
+        cursorCategorias = getCategorias(tabelaCategorias);
+        assertEquals(3, cursorCategorias.getCount());
+
+        tabelaCategorias.delete(BdTableCategories._ID + "=?", new String[]{String.valueOf(id)});
+        cursorCategorias = getCategorias(tabelaCategorias);
+        assertEquals(2, cursorCategorias.getCount());
+
+        getCategoriaComID(cursorCategorias, idHistoria);
+        getCategoriaComID(cursorCategorias, idDesporto);
+
+
+    }
+    private long criaCategoria(BdTableCategories tabelaCategorias, String nome) {
+        Categories categoria = new Categories();
+        categoria.setDescricao(nome);
+
+        long id = tabelaCategorias.insert(categoria.getContentValues());
+        assertNotEquals(-1, id);
+
+        return id;
+    }
+    private Cursor getCategorias(BdTableCategories tabelaCategorias) {
+        return tabelaCategorias.query(BdTableCategories.TODAS_COLUNAS, null, null, null, null, null);
+    }
+    private Categories getCategoriaComID(Cursor cursor, long id) {
+        Categories categoria = null;
+
+        while (cursor.moveToNext()) {
+            categoria = Categories.fromCursor(cursor);
+
+            if (categoria.getId() == id) {
+                break;
+            }
+        }
+
+        assertNotNull(categoria);
+
+        return categoria;
     }
 }
